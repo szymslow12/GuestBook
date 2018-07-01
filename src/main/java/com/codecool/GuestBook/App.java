c074a644c04091bb80e47098faf76386fa5d178e
szymslow12 <szym.slow12@gmail.com>
2018-07-01 18:15:12 +0200

    change /static/guests.txt content to /static/guestbook.xml

--- src/main/java/com/codecool/GuestBook/App.java
+++ src/main/java/com/codecool/GuestBook/App.java
@@ -1,9 +1,7 @@
 package com.codecool.GuestBook;
 
-import com.codecool.GuestBook.model.DataReader;
 import com.sun.net.httpserver.HttpServer;
 import java.net.InetSocketAddress;
-
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -11,8 +9,8 @@ public class App {
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
 
         // set routes
-        DataReader dataReader = new DataReader();
-        server.createContext("/static/guests.txt", new Guests());
+
+        server.createContext("/static/guestbook.xml", new Guests());
         server.createContext("/static", new Static());
         server.setExecutor(null); // creates a default executor
 
