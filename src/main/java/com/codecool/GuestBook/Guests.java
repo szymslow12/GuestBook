2f106c0ec8c68d7f002d2955a60933a4c158a677
szymslow12 <szym.slow12@gmail.com>
2018-08-20 19:50:26 +0200

    remove old Guest body -> now sends JSON

--- src/main/java/com/codecool/GuestBook/Guests.java
+++ src/main/java/com/codecool/GuestBook/Guests.java
@@ -1,75 +1,44 @@
 package com.codecool.GuestBook;
 
-import com.codecool.GuestBook.helpers.MimeTypeResolver;
-import com.codecool.GuestBook.model.DatabaseDataReader;
-import com.codecool.GuestBook.model.XMLParser;
-import com.sun.net.httpserver.Headers;
+
+import com.codecool.GuestBook.model.Guest;
+import com.codecool.GuestBook.model.GuestDAO;
+
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
-import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URL;
-import java.util.Scanner;
 
-public class Guests implements HttpHandler{
+import java.util.Iterator;
 
-    public Guests() {
-        DatabaseDataReader dataReader = new DatabaseDataReader();
-        XMLParser xmlParser = new XMLParser();
-        xmlParser.writeGuestsToXML(dataReader.getGuests().getIterator());
-    }
+public class Guests implements HttpHandler {
 
     public void handle(HttpExchange httpExchange) throws IOException {
+        String response = getResponse();
 
-        // get file path from url
-        URI uri = httpExchange.getRequestURI();
+        httpExchange.getResponseHeaders().set("Content-Type", "application/json");
+        httpExchange.sendResponseHeaders(200, response.length());
 
-        String path = "." + uri.getPath();
-        System.out.println("looking for: " + uri.getPath() + "\u001B[35m PATH: " + path + "\u001B[0m" );
-        ClassLoader classLoader = getClass().getClassLoader();
-        URL fileURL = classLoader.getResource(path);
-
-        //OutputStream os = httpExchange.getResponseBody();
-
-        if (fileURL == null) {
-            // Object does not exist or is not a file: reject with 404 error.
-            send404(httpExchange);
-        } else {
-            // Object exists and is a file: accept with response code 200.
-            sendFile(httpExchange, fileURL);
-        }
-
-    }
-
-    private void send404(HttpExchange httpExchange) throws IOException {
-        String response = "404 (Not Found)\n";
-        httpExchange.sendResponseHeaders(404, (long)response.length());
         OutputStream os = httpExchange.getResponseBody();
         os.write(response.toString().getBytes());
         os.close();
     }
 
-    private void sendFile(HttpExchange httpExchange, URL fileURL) throws IOException {
-        File file = new File(fileURL.getFile());
-        System.out.println("fileURL: " + fileURL + " file: " + file);
-        httpExchange.getResponseHeaders().set("Content-Type", "application/xml");
-        httpExchange.sendResponseHeaders(200, file.length());
-        OutputStream os = httpExchange.getResponseBody();
-        FileInputStream fs = new FileInputStream(file);
-        final byte[] buffer = new byte[0x10000];
-
-        int count;
-        while((count = fs.read(buffer)) >= 0) {
-            System.out.println(buffer + " buffer || " + count + " count");
-            os.write(buffer, 0, count);
+    private String getResponse() {
+        GuestDAO dataReader = new GuestDAO();
+        Iterator<Guest> guestIterator = dataReader.getGuests().getIterator();
+        JSONArray array = new JSONArray();
+        while (guestIterator.hasNext()) {
+            Guest guest = guestIterator.next();
+            JSONObject object = new JSONObject();
+            object.put("name", guest.getName());
+            object.put("message", guest.getMessage());
+            object.put("date", guest.getDate());
+            array.put(object);
         }
-
-        os.close();
+        return array.toString();
     }
 }
