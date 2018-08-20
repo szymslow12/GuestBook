54bacc7d5298e779bb135209b335887e44d7c045
szymslow12 <szym.slow12@gmail.com>
2018-08-20 20:07:29 +0200

    remove unused methods, remove System.outs

--- src/main/java/com/codecool/GuestBook/Static.java
+++ src/main/java/com/codecool/GuestBook/Static.java
@@ -54,9 +54,7 @@ public class Static implements HttpHandler {
         File file = new File(fileURL.getFile());
         // we need to find out the mime type of the file, see: https://en.wikipedia.org/wiki/Media_type
         MimeTypeResolver resolver = new MimeTypeResolver(file);
-        System.out.println("fileURL: " + fileURL + " file: " + file);
         String mime = resolver.getMimeType();
-        System.out.println(" mime resolver.getMimeType()");
 
         //httpExchange.getResponseHeaders().set("Content-Type", mime);
         httpExchange.sendResponseHeaders(200, file.length());
@@ -68,72 +66,9 @@ public class Static implements HttpHandler {
         final byte[] buffer = new byte[0x10000];
         int count = 0;
         while ((count = fs.read(buffer)) >= 0) {
-            System.out.println(buffer + " buffer || " + count + " count");
             os.write(buffer,0,count);
         }
         os.close();
-    }
-
-    private void checkFile(File file) {
-        if (file.isFile()) {
-            System.out.println("\u001B[31m File is a file \u001B[0m");
-        } else if (file.isDirectory()) {
-            System.out.println("\u001B[31m File is a directory \u001B[0m");
-            checkFilesInDirectory(file);
-        }
-    }
-
-    private void checkFilesInDirectory(File file) {
-        String[] filesInDirectory = file.list();
-        System.out.println("\u001B[31m File in directory:");
-        for (int i = 0; i < filesInDirectory.length; i++) {
-            System.out.println(i + ". " + filesInDirectory[i] + "\u001B[0m");
-        }
-    }
-
-    private void sendHTMLFile(HttpExchange httpExchange, URL fileURL) throws IOException {
-        sendFile(httpExchange, getHTMLFileURL(fileURL));
-        //sendFile(httpExchange, getPathToCSS(fileURL));
-
-    }
-
-
-    private URL getHTMLFileURL(URL fileURL) {
-        File file = new File(fileURL.getFile());
-        //checkFile(file);
-        try {
-            return new URL(getAbsolutePathToFile(file, fileURL.toString(), ".html"));
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        System.out.println("error");
-        return fileURL;
-    }
-
-    private String getAbsolutePathToFile(File file, String fileURL, String matcher) {
-        String[] filesInDirectory = file.list();
-        for (int i = 0; i < filesInDirectory.length; i++) {
-            if (filesInDirectory[i].contains(matcher)) {
-                return fileURL + "/" + filesInDirectory[i];
-            }
-        }
-        return fileURL;
-    }
-
-    private URL getPathToCSS(URL fileURL) {
-        String filePath = fileURL.getFile();
-        File file = new File(filePath);
-        String path = getAbsolutePathToFile(file, fileURL.toString(), "css");
-        System.out.println(path + "    css");
-        try {
-            String pathToCSS = getAbsolutePathToFile(new File(new URL(path).getFile()), path, ".css");
-            System.out.println(pathToCSS + " zadzialaj");
-            return new URL(pathToCSS);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        System.out.println("error");
-        return fileURL;
     }
 }
 
