ec767eeac3a12795a36eb91d0f21cad304101292
szymslow12 <szym.slow12@gmail.com>
2018-08-20 19:49:50 +0200

    remove coments, add context /guestBook, change context /static/guestbook.xml -> /guests

--- src/main/java/com/codecool/GuestBook/App.java
+++ src/main/java/com/codecool/GuestBook/App.java
@@ -1,20 +1,20 @@
 package com.codecool.GuestBook;
 
+import com.codecool.GuestBook.controller.GuestBook;
 import com.sun.net.httpserver.HttpServer;
 import java.net.InetSocketAddress;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        // create a server on port 8000
+
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
 
-        // set routes
+        server.createContext("/guests", new Guests());
 
-        server.createContext("/static/guestbook.xml", new Guests());
         server.createContext("/static", new Static());
-        server.setExecutor(null); // creates a default executor
+        server.createContext("/guestBook", new GuestBook());
+        server.setExecutor(null);
 
-        // start listening
         server.start();
     }
 }
