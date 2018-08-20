38c2537ae13fde2be4d28e3d75de4b4cdea5f2ee
szymslow12 <szym.slow12@gmail.com>
2018-08-20 19:52:28 +0200

    renamge DatabaseDataReader -> GuestDAO, add sendNewGuestToDatabase

--- src/main/java/com/codecool/GuestBook/model/GuestDAO.java
+++ src/main/java/com/codecool/GuestBook/model/GuestDAO.java
@@ -1,17 +1,22 @@
 package com.codecool.GuestBook.model;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.Map;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Iterator;
 
-public class DatabaseDataReader {
+public class GuestDAO {
 
     private Connection connection;
     private GuestRepository guests;
 
-    public DatabaseDataReader() {
+    public GuestDAO() {
         connection = new DatabaseConnection().connect(" ");
         guests = new GuestRepository();
         loadGuestsFromDatabase();
@@ -20,11 +25,12 @@ public class DatabaseDataReader {
 
     private void loadGuestsFromDatabase() {
         try {
-            PreparedStatement preparedStatement = connection.prepareStatement("Select * from guests");
+            PreparedStatement preparedStatement = connection.prepareStatement("Select * FROM guests ORDER BY id_guest");
             ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
                 guests.addGuest(getGuestFromDatabse(resultSet));
             }
+            showGuests();
         } catch (SQLException err) {
             err.printStackTrace();
         }
@@ -50,5 +56,20 @@ public class DatabaseDataReader {
 
     public GuestRepository getGuests() {
         return guests;
+    }
+
+    public void sendNewGuestToDatabase(Map<String, Object> guest) {
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO guests(guest_name, message, visit_date) VALUES(?, ? ,?)");
+            for (int i = 0; i < guest.keySet().size(); i++) {
+                String key = (String) guest.keySet().toArray()[i];
+                preparedStatement.setString(i + 1, (String) guest.get(key));
+            }
+            preparedStatement.setString(3, LocalDate.now().toString() + " " + LocalTime.now().toString());
+            preparedStatement.execute();
+        } catch (SQLException err) {
+            err.printStackTrace();
+        }
+
     }
 }
